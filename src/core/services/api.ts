f2a0b27fb412cad131c77f24208498bb0331aa0e
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-09-06 15:50:13 -0300

    feat: create api client

---
+++ src/core/services/api.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from "axios";
+import { parseCookies } from "nookies";
+
+import { signOut } from "../contexts/AuthContext";
+import { AuthTokenError } from "./errors/AuthTokenError";
+
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+  const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      Authorization: `Bearer ${cookies["nextauth.token"]}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error: AxiosError) => {
+      if (error.response && error.response.status === 401) {
+        rejectAuth();
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return api;
+}
+
+const rejectAuth = () => {
+  if (window) {
+    signOut();
+  } else {
+    return Promise.reject(new AuthTokenError());
+  }
+};
