ca6cf375139cdbbd716e6561f41fe370d4f36c8d
Rodrigo Anselmo <rodrigoanselmo.dev@gmail.com>
2024-07-18 16:50:46 -0300

    fix: remove alal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <DefaultProviders>
-      <DefaultLayout>
         <Component {...pageProps} />
-      </DefaultLayout>
-    </DefaultProviders>
   );
 }
 
